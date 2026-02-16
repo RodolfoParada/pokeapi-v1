fa3b9fad7e5be65707c47638b4053a89b265891f
Rodolfo Parada <rodolfo.parada.gonzalez>
2026-02-16 12:25:39 -0300

    feat: modificación en css

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function navegar(ruta) {
     
     else if (ruta === "pokedex") {
         mainContent.innerHTML = `
-            <input type="text" id="buscador" placeholder="Buscar..." style="padding:8px; width:250px; margin-bottom:20px;">
+            <input type="text" id="buscador" placeholder="Buscar por nombre, id o tipo..." style="padding:8px; width:250px; margin-bottom:20px;">
             <div id="pokedex" style="display: flex; flex-wrap: wrap; gap: 20px; justify-content: center;"></div>
             <div id="paginacion" style="margin-top:20px; text-align: center;"></div>`;
         
