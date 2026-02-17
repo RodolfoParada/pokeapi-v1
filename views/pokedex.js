f39726dd17aeafbd80c3089b228040ac3d47ad38
Rodolfo Parada <rodolfo.parada.gonzalez>
2026-02-17 12:07:00 -0300

    feat: responisve

--- views/pokedex.js
+++ views/pokedex.js
@@ -4,8 +4,7 @@ export function renderPokedexLayout() {
             <input type="text" id="buscador" placeholder="Buscar por nombre, id o tipo..." 
                    style="padding:10px; width:400px; margin-bottom:20px; border-radius: 8px; border: 1px solid #ccc;">
             
-            <div id="pokedex" 
-            "></div>
+            <div id="pokedex"></div>
             
             <div id="paginacion" style="margin-top:20px; display: flex; justify-content: center; align-items: center;"></div>
         </div>
