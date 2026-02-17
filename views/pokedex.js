93b62f7fcb3d61be1de79a44ea45ca8989025aeb
Rodolfo Parada <rodolfo.parada.gonzalez>
2026-02-17 11:50:02 -0300

    feat: mejora del responsive

--- views/pokedex.js
+++ views/pokedex.js
@@ -4,12 +4,7 @@ export function renderPokedexLayout() {
             <input type="text" id="buscador" placeholder="Buscar por nombre, id o tipo..." 
                    style="padding:10px; width:400px; margin-bottom:20px; border-radius: 8px; border: 1px solid #ccc;">
             
-            <div id="pokedex" style="
-                display: grid; 
-                grid-template-columns: repeat(10, 1fr); 
-                gap: 15px; 
-                justify-items: center;
-                min-height: 500px;
+            <div id="pokedex" 
             "></div>
             
             <div id="paginacion" style="margin-top:20px; display: flex; justify-content: center; align-items: center;"></div>
